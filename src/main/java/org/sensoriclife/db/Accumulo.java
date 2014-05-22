c9ab910064f7f4b3cae81726116049bffd3ed83f
Marcel <m.jacob@hotmail.com>
2014-05-22 18:15:08 +0200

    mockInstance name, getConnector added

--- src/main/java/org/sensoriclife/db/Accumulo.java
+++ src/main/java/org/sensoriclife/db/Accumulo.java
@@ -80,8 +80,8 @@ public class Accumulo {
 	 * @return scanner
 	 * @throws TableNotFoundException
 	 */
-	public synchronized Scanner getScannder(String table) throws TableNotFoundException {
-		return this.getScannder(table, Authorizations.EMPTY);
+	public synchronized Scanner getScanner(String table) throws TableNotFoundException {
+		return this.getScanner(table, Authorizations.EMPTY);
 	}
 
 	/**
@@ -91,8 +91,8 @@ public class Accumulo {
 	 * @return scanner
 	 * @throws TableNotFoundException
 	 */
-	public synchronized Scanner getScannder(String table, String visibility) throws TableNotFoundException {
-		return this.getScannder(table, Authorizations.EMPTY);
+	public synchronized Scanner getScanner(String table, String visibility) throws TableNotFoundException {
+		return this.getScanner(table, Authorizations.EMPTY);
 	}
 
 	/**
@@ -102,7 +102,7 @@ public class Accumulo {
 	 * @return scanner
 	 * @throws TableNotFoundException
 	 */
-	public synchronized Scanner getScannder(String table, Authorizations auths) throws TableNotFoundException {
+	public synchronized Scanner getScanner(String table, Authorizations auths) throws TableNotFoundException {
 		return this.connector.createScanner(table, auths);
 	}
 
@@ -112,7 +112,7 @@ public class Accumulo {
 	 * @throws AccumuloSecurityException
 	 */
 	public synchronized void connect() throws AccumuloException, AccumuloSecurityException {
-		this.instance = new MockInstance();
+		this.instance = new MockInstance("mockInstance");
 		this.connector = this.instance.getConnector("",  new PasswordToken(""));
 	}
 
@@ -157,6 +157,18 @@ public class Accumulo {
 	public synchronized void connect(String name, String zooServers, String user, String password) throws AccumuloException, AccumuloSecurityException {
 		this.instance = new ZooKeeperInstance(name, zooServers);
 		this.connector = this.instance.getConnector(user, new PasswordToken(password));
+	}
+	
+	/**
+	 * returns the connector
+	 * @return Connector
+	 */
+	public Connector getConnector() {
+		return connector;
+	}
+	
+	public MockInstance getMockInstance(){
+		return (MockInstance) instance;
 	}
 
 	/**
@@ -244,7 +256,8 @@ public class Accumulo {
 	 * @throws TableNotFoundException
 	 */
 	public synchronized Iterator<Entry<Key,Value>> scanAll(String table) throws TableNotFoundException {
-		return this.scanAll(table, Authorizations.EMPTY);
+		//return this.scanAll(table, Authorizations.EMPTY);
+		return this.scanAll(table, new Authorizations());
 	}
 
 	/**
